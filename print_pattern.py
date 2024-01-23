7ee02a1d340f2bcd040f1e668b9c14ef80c1b1f5
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 23:42:44 +0800

    Print the pattern

--- print_pattern.py
+++ print_pattern.py
@@ -4,8 +4,10 @@
 
 # Create a loop for row
 for number in range(1, 6):
+    
 # Create a loop for column
     for numbers in range (1, number+1):
        
-
 # Print the pattern 
+         print(number, end="")
+    print()
